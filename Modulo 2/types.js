71ff79bf76efa7c78d85c9a43f2a430fddfa806a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-24 16:37:46 -0300

    add types

---
+++ Modulo 2/types.js
@@ -0,0 +1,13 @@
+"use strict";
+// Number
+const numero = 10;
+// String
+const nome = "Leonardo";
+// Object
+let objeto;
+// Function
+const variavel = 'função';
+function função(variavel) {
+    return `Isto é uma ${variavel}`;
+}
+console.log(função(variavel));
---
+++ Modulo 2/types.ts
@@ -0,0 +1,23 @@
+// Number
+const numero: number = 10;
+
+// String
+const nome: string = "Leonardo";
+
+
+// Object
+let objeto: {
+  nome: string;
+  endereço: string;
+  telefone: number;
+}
+
+// Function
+const variavel = 'função';
+
+function função(variavel: string) {
+  return `Isto é uma ${variavel}`
+}
+
+console.log(função(variavel));
+
