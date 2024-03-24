8bc3b7c382ac08ae7d69bb84379998909b82382e
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-24 16:39:37 -0300

    add boolean

---
+++ Modulo 2/boolean.js
@@ -0,0 +1,9 @@
+"use strict";
+// Boolean
+const terminou = false;
+if (terminou) {
+    console.log('Terminou');
+}
+if (!terminou) {
+    console.log('Não Terminou');
+}
---
+++ Modulo 2/boolean.ts
@@ -0,0 +1,5 @@
+// Boolean
+const terminou: boolean = false
+
+if(terminou) { console.log('Terminou') }
+if(!terminou) { console.log('Não Terminou') }
