9f1ddc7b474f1a3a60930f91d90a15c5152dc4d5
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-25 17:11:27 -0300

    add bigint file

---
+++ Modulo 2/bigint.js
@@ -0,0 +1,17 @@
+"use strict";
+const dec = 226.71239281913443619487225078046321868896484375000000;
+const hex = 0xf5bebe9927ce8c9ad6e5ca3cbdfa97d7;
+const oct = 10130715031124016422065364234036;
+const bin = 10101110000011000111101011001100;
+const bigdec = 22671239281913443619487225078046321868896484375000000n;
+const bighex = 0xf5bebe9927ce8c9ad6e5ca3cbdfa97d7n;
+const bigoct = 10130715031124016422065364234036n;
+const bigbin = 10101110000011000111101011001100n;
+console.log(dec);
+console.log(hex);
+console.log(oct);
+console.log(bin);
+console.log(bigdec);
+console.log(bighex);
+console.log(bigoct);
+console.log(bigbin);
---
+++ Modulo 2/bigint.ts
@@ -0,0 +1,18 @@
+const dec: number = 226.71239281913443619487225078046321868896484375000000;
+const hex: number = 0xf5bebe9927ce8c9ad6e5ca3cbdfa97d7;
+const oct: number = 10130715031124016422065364234036;
+const bin: number = 10101110000011000111101011001100;
+
+const bigdec: bigint = 22671239281913443619487225078046321868896484375000000n;
+const bighex: bigint = 0xf5bebe9927ce8c9ad6e5ca3cbdfa97d7n;
+const bigoct: bigint = 10130715031124016422065364234036n;
+const bigbin: bigint = 10101110000011000111101011001100n;
+
+console.log(dec)
+console.log(hex)
+console.log(oct)
+console.log(bin)
+console.log(bigdec)
+console.log(bighex)
+console.log(bigoct)
+console.log(bigbin)
