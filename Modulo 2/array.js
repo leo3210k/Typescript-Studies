90fbf3698b184f8f9dae79d31e098f97020b3d95
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-26 17:17:16 -0300

    add array file

---
+++ Modulo 2/array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Array
+let lista = ['Leo', 'Lucas', 'Lucien', 'Liziane'];
+let lista1 = ['Leo', 'Lucas', 'Lucien', 'Liziane'];
+let lista2 = new Array('Leo', 'Lucas', 'Lucien', 'Liziane');
+// push
+lista.push('Mel');
+console.log(lista);
+// length
+console.log(lista.length);
---
+++ Modulo 2/array.ts
@@ -0,0 +1,11 @@
+// Array
+let lista: string[] = ['Leo', 'Lucas', 'Lucien', 'Liziane']
+let lista1: Array<string> = ['Leo', 'Lucas', 'Lucien', 'Liziane']
+let lista2: string[] = new Array('Leo', 'Lucas', 'Lucien', 'Liziane')
+
+// push
+lista.push('Mel')
+console.log(lista)
+
+// length
+console.log(lista.length)
