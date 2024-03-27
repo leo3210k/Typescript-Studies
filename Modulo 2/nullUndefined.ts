c2d798195764bb019e4746fc8d1b3833dccd2101
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-27 17:06:36 -0300

    add null undefined file

---
+++ Modulo 2/nullUndefined.ts
@@ -0,0 +1,18 @@
+// ==> Exemplo 01: Null
+
+let variavelTesteNull = null;
+
+console.log(variavelTesteNull);
+console.log(typeof variavelTesteNull);
+
+// ==> Exemplo 02: Undefined
+
+let variavelTesteUndefined;
+
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+// ==> Diferenças e Similaridades: Null vs Undefined
+
+console.log('Exemplo 01:', null == undefined);
+console.log('Exemplo 02:', null === undefined);
