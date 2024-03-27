ae273b33d42e42be3c566204e4ba0b5be001ae4d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-26 21:19:01 -0300

    add enum file

---
+++ Modulo 2/enum.js
@@ -0,0 +1,19 @@
+"use strict";
+// Enum
+var Idioma;
+(function (Idioma) {
+    Idioma[Idioma["Portugu\u00EAs"] = 0] = "Portugu\u00EAs";
+    Idioma[Idioma["Ingl\u00EAs"] = 1] = "Ingl\u00EAs";
+    Idioma[Idioma["Japon\u00EAs"] = 2] = "Japon\u00EAs";
+    Idioma[Idioma["Italiano"] = 3] = "Italiano";
+})(Idioma || (Idioma = {}));
+;
+console.log(Idioma);
+var Idioma2;
+(function (Idioma2) {
+    Idioma2["Portugu\u00EAs"] = "PT-BR";
+    Idioma2["Ingl\u00EAs"] = "ES";
+    Idioma2["Japon\u00EAs"] = "JP";
+    Idioma2["Italiano"] = "IT";
+})(Idioma2 || (Idioma2 = {}));
+console.log(Idioma2);
---
+++ Modulo 2/enum.ts
@@ -0,0 +1,18 @@
+// Enum
+enum Idioma {
+  'Português',
+  'Inglês',
+  'Japonês',
+  'Italiano',
+};
+
+console.log(Idioma);
+
+enum Idioma2 {
+  'Português' = 'PT-BR',
+  'Inglês' = 'ES',
+  'Japonês' = 'JP',
+  'Italiano' = 'IT'
+}
+
+console.log(Idioma2)
