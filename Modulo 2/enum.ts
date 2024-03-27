d0f37d48c9f8dc89fb7b6e5f60b4af884d866a28
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-26 21:50:59 -0300

    add more enum content

--- Modulo 2/enum.ts
+++ Modulo 2/enum.ts
@@ -1,4 +1,5 @@
-// Enum
+// ==> Exemplo 01: Numeric Enums (Enums Numérico)
+// Cuidado ao usar o enum!!!
 enum Idioma {
   'Português',
   'Inglês',
@@ -8,6 +9,9 @@ enum Idioma {
 
 console.log(Idioma);
 
+// ==> Exemplo 02: String Enums
+// O livro Programming Typescript do Boris Cherny deixa claro que você pode até usar
+// enum em string mas não é recomendado, procurar evitar
 enum Idioma2 {
   'Português' = 'PT-BR',
   'Inglês' = 'ES',
@@ -16,3 +20,40 @@ enum Idioma2 {
 }
 
 console.log(Idioma2)
+
+// ==> Exemplo 03: Como podemos acessar um valor de um Enum com uma chave: (usando o const)
+
+const enum Comida {
+  Hamburger,
+  Massa,
+  Pizza,
+  Torta,
+  Churrasco,
+}
+
+function comida(c: Comida) {
+  return 'Comidas muito apetitosas!';
+}
+
+console.log(comida(Comida.Pizza));
+console.log(comida(Comida.Churrasco));
+// console.log(comida(5));
+
+
+// ==> Exemplo 04: Quando usar enum?!
+enum Tarefa {
+  Todo,
+  Progress,
+  Done,
+}
+
+// Quando usar enum?!
+const concluidaTarefa = {
+  id: 1,
+  status: Tarefa.Done,
+  descricao: 'Parabéns! Tarefa concluída com sucesso!',
+};
+
+if (concluidaTarefa.status === Tarefa.Done) {
+  console.log('Enviar e-mail: Tarefa Concluída!');
+}
