779b0af45098d9169b22e0bc59dfcb529ce588d9
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-27 21:03:37 -0300

    add object type part 2

--- Modulo 2/objectType.ts
+++ Modulo 2/objectType.ts
@@ -82,3 +82,105 @@ function onboarding04(pessoa: Pessoa04) {
 }
 
 console.log(onboarding04({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/Typescript' }));
+
+// ==> Exemplo 06 -> Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado objeto use o '
+//readonly)
+interface Pessoa05 {
+  nome: string;
+  funcao: string;
+  linguagem: string;
+  readonly email: string;
+}
+
+function onboarding05(pessoa: Pessoa05) {
+  return (
+    'Seja bem-vinda ' +
+    pessoa.nome +
+    '!' +
+    ' Sua função aqui na empresa será ' +
+    pessoa.funcao +
+    '.' +
+    ' Você trabalhará com a linguagem ' +
+    pessoa.linguagem +
+    '.' +
+    ' Seu e-mail será ' +
+    pessoa.email
+  );
+}
+
+console.log(onboarding05(
+  {
+    nome: 'Glaucia Lemos',
+    funcao: 'Cloud Advocate',
+    linguagem: 'JavaScript/Typescript',
+    email: 'gllemos@microsoft.com'
+  }
+));
+
+// ==> Exemplo 07 -> tipos de extensões (heranças)
+interface Mae {
+  nome: string;
+}
+
+interface Pai {
+  sobrenome: string;
+}
+
+interface Filha extends Mae, Pai {
+  idade: number;
+}
+
+const filha: Filha = {
+  nome: 'Glaucia',
+  sobrenome: 'Lemos',
+  idade: 35
+}
+
+console.log(filha);
+
+// ==> Exemplo 08 -> Tipos de Interseções
+interface Cachorro {
+  tipo: string;
+}
+
+interface Gato {
+  tipo: string;
+}
+
+type Animal = Cachorro & Gato;
+
+// Exemplo 09 - Generic Objects
+type Usuario = {
+  nome: string;
+  email: string;
+}
+
+type Admin = {
+  nome: string;
+  email: string;
+  admin: true;
+}
+
+const usuario: Usuario = {
+  nome: 'Glaucia Lemos',
+  email: 'algumacoisa@gmail.com'
+}
+
+const admin: Admin = {
+  nome: 'Glaucia Lemos',
+  email: 'algumacoisa@gmail.com',
+  admin: true
+}
+
+function acessarSistema<T>(usuario: T): T {
+  return usuario;
+}
+
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admin>(admin));
+
+/*function acessarSistema(usuario: Usuario): Usuario {
+  return usuario;
+};*/
+
+// console.log(acessarSistema(usuario));
