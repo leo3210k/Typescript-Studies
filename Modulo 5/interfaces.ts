9c4227b3986c5ce1ff1b149df0abe1f88db25bc3
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-01 10:38:37 -0300

    add comments to interfaces file

---
+++ Modulo 5/interfaces.ts
@@ -0,0 +1,8 @@
+/*
+// Contrato sintático
+// Entidade deve obedecer
+// As interfaces definem: propriedades, métodos e eventos
+// Declaração de membros, sendo responsabilidade
+// da entidade derivada definir os membros
+// Estrutura padrão que as classes derivadas deveriam seguir
+*/
