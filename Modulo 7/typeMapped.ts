d47f24f173d187984bbd886b552728a7f2059a87
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-03 16:59:46 -0300

    add type mapped file content

--- Modulo 7/typeMapped.ts
+++ Modulo 7/typeMapped.ts
@@ -1,3 +1,63 @@
+export { };
+
 // Obter um modelo existente
 // Transformar cada propriedade num novo tipo
 // Permanecer sincronizado com outro tipo
+
+// ==> Exemplo 01: Mapped Types
+type Usuario = {
+  nome: string;
+  endereco: string;
+  telefone: string;
+  idade: number;
+};
+
+type UsuarioOpcional = {
+  nome?: string;
+  endereco?: string;
+  telefone?: string;
+  idade?: number;
+};
+
+type UsusarioSomenteLeitura = {
+  readonly nome: string;
+  readonly endereco: string;
+  readonly telefone: string;
+  readonly idade: number;
+};
+
+// { [P in K]: T }
+
+type UsuarioMappedType = {
+  [P in keyof Usuario]?: Usuario[P]; // for in
+};
+
+// 'nome' | 'endereco' | 'telefone' | 'idade'
+
+const usuarioMapped: UsuarioMappedType = {
+  nome: 'Glaucia Lemos',
+  endereco: 'Rua 01, 123',
+}
+
+console.log(usuarioMapped);
+
+// ==> Exemplo 02: Outros Tipos Avançados
+interface Livro {
+  titulo: string;
+  autor: string | null;
+  preco: number;
+  numeroPaginas: number;
+}
+
+type Artigo = Omit<Livro, 'numeroPaginas'>;
+
+type LivroModelo = Readonly<Livro>;
+
+const livro: LivroModelo = {
+  autor: 'Boris Cherny',
+  numeroPaginas: 324,
+  preco: 19.99,
+  titulo: 'Programming Typescript: Making Your JavaScript Applications Scale'
+}
+
+// livro.autor = "Glaucia Lemos"
