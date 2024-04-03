d3bbe1240056e5a7ccc676b2d2c37148283807b4
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-03 11:23:13 -0300

    add type conditional file content

---
+++ Modulo 7/typeConditional.ts
@@ -0,0 +1,51 @@
+export = {};
+
+
+// ==> Exemplo 01 - Conditional Types
+type Pessoa = {
+  nome: string;
+  idade: number;
+  endereco: string;
+};
+
+type Empresa = {
+  nome: string;
+  cnpj: number;
+};
+
+type EnderecoPessoa = {
+  enderecoSecundario: string;
+  cidade: string;
+  pais: string;
+};
+
+type EnderecoEmpresa = {
+  localizacao: 'rua' | 'avenida' | 'praça';
+};
+
+type EnderecoFinal<T> = T extends { endereco: string } ? EnderecoPessoa : EnderecoEmpresa;
+
+const enderecoPessoa: EnderecoFinal<Pessoa> = {
+  enderecoSecundario: 'Rua 2',
+  cidade: 'São Paulo',
+  pais: 'Brasil',
+};
+
+const enderecoEmpresa: EnderecoFinal<Empresa> = {
+  localizacao: 'rua',
+};
+
+console.log(enderecoPessoa);
+console.log(enderecoEmpresa);
+
+// ==> Exemplo 02 - Conditional Types ()
+
+type FormatoArquivos = 'png' | 'jpg' | 'gif' | 'svg' | 'mp4' | 'mp3';
+
+type FiltrarArquivoAudio<T> = T extends 'mp4' | 'mp3' ? T : never;
+
+type ArquivoAudio = FiltrarArquivoAudio<FormatoArquivos>;
+
+const arquivoAudio: ArquivoAudio = 'mp4';
+
+console.log(arquivoAudio);
