a48f867ca7e7b1b1d119a8e193782399107b8a5b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-03 11:00:09 -0300

    add type assertion file content

---
+++ Modulo 7/typeAssertion.ts
@@ -0,0 +1,44 @@
+export { };
+
+// Type Assertion instrui o compilador a tratar um valor
+// com o tipo especificado, não carrega conversão de tipo
+// ==> Exemplo 01 - Type Assertions (as / < >)
+function exibirPrecoFinal(preco: number, desconto: number, formato: boolean): number | string {
+  const precoComDesconto = preco * (1 - desconto);
+
+  return formato ? `R$ ${precoComDesconto}` : precoComDesconto;
+}
+
+// const descontoFinal = exibirPrecoFinal(100, 0.05, true) as string;
+const descontoFinal = <number>exibirPrecoFinal(100, 0.05, false);
+console.log(descontoFinal);
+
+// ==> Exemplo 02 - Type Assertions
+
+/*type Humano = {
+  nome: string;
+  idade: number;
+  idioma: string;
+}
+
+const humano = {
+  idade: 36,
+  idioma: 'Português'
+};
+
+const humano_02 = humano as Humano;
+console.log(humano_02.nome.toUpperCase());*/
+
+/*type Humano = {
+  nome: string;
+  idade: number;
+  idioma: string;
+}
+
+const humano = {
+  idade: 36,
+  idioma: 'Português'
+};
+
+const humano_02: Humano = humano;
+console.log(humano_02.nome.toUpperCase());*/
