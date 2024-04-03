053469d3fbdeeff65d537aa3f33da732290dca30
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-03 19:09:39 -0300

    add comments to type satisfies file

---
+++ Modulo 7/typeSatisfies.ts
@@ -0,0 +1,6 @@
+/*
+// O operador satisfaz informa se um determinado tipo satisfaz uma 
+// condição específica – e fornece essas informações antes de executar 
+// seu código. Além disso, ao usá-lo, você pode declarar uma nova variável 
+// para verificar se o tipo de uma expressão corresponde a outro tipo.
+*/
