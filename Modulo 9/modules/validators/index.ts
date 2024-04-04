7eef3a7cc2fa47d672d4574bd7f5a7a111f9ee28
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-04 16:37:34 -0300

    add index file

---
+++ Modulo 9/modules/validators/index.ts
@@ -0,0 +1,5 @@
+import { Validator } from "./validator";
+import EmailValidator from "./emailValidator";
+import { ZipCodeValidator } from "./zipcodeValidator";
+
+export { Validator, EmailValidator, ZipCodeValidator };
