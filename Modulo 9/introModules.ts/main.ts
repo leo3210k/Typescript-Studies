2c9a1574f937a320c519c47a1d11c3cd4c855399
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-04 16:33:45 -0300

    add main file

---
+++ Modulo 9/introModules.ts/main.ts
@@ -0,0 +1,6 @@
+import { somar, subtrair, multiplicar, dividir } from './math';
+
+console.log(somar(10, 20));
+console.log(subtrair(10, 20));
+console.log(multiplicar(10, 20));
+console.log(dividir(10, 20));
