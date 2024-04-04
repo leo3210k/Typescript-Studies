d86773887175c15577a24fb4f3d0b96961079198
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-03 21:26:53 -0300

    add generics constraints file content

---
+++ Modulo 8/genericsConstraints.ts
@@ -0,0 +1,81 @@
+export { };
+
+// Podemos restringir certos tipos usando constraints
+
+// ==> Exemplo 01 - Generics Constraints
+interface Pessoa {
+  nome: string;
+  idade: number;
+}
+
+function obterPessoaIdadeMaiorQue<T extends Pessoa>(pessoas: T[], idade: number): T[] {
+  return pessoas.filter(pessoa => pessoa.idade > idade);
+}
+
+const pessoas: Pessoa[] = [
+  { nome: 'Glaucia', idade: 36 },
+  { nome: 'João', idade: 20 },
+  { nome: 'Angelica', idade: 30 },
+  { nome: 'Maria', idade: 25 },
+  { nome: 'José', idade: 18 },
+];
+
+const pessoasComIdadeMaiorQue25 = obterPessoaIdadeMaiorQue(pessoas, 25);
+
+console.log(pessoasComIdadeMaiorQue25);
+
+// ==> Exemplo 02 - Generics Constraints
+function juntarObjetos<U, V>(objeto1: U, objeto2: V) {
+  return {
+    ...objeto1,
+    ...objeto2,
+  }
+}
+
+const pessoa = juntarObjetos(
+  { nome: 'Glaucia', },
+  { idade: 36, }
+);
+
+console.log(pessoa);
+
+const pessoa2 = juntarObjetos(
+  { nome: 'Glaucia' },
+  36
+);
+
+console.log(pessoa2);
+
+function juntarObjetos2<U extends object, V extends object>(objeto1: U, objeto2: V) {
+  return {
+    ...objeto1,
+    ...objeto2,
+  }
+}
+
+const pessoa3 = juntarObjetos2(
+  { nome: 'Glaucia' },
+  { idade: 36 }
+);
+
+// ==> Exemplo 03 - Type parameter in generic constraints
+
+/*function prop<T, K>(objeto: T, chave: K) {
+  return objeto[chave];
+}*/
+
+function prop2<T, K extends keyof T>(objeto: T, chave: K) {
+  return objeto[chave];
+}
+
+const pessoa4 = prop2(
+  { nome: 'Glaucia' }, 'nome'
+);
+
+console.log(pessoa4);
+
+/*const pessoa5 = prop2(
+  { nome: 'Glaucia' }, 'idade'
+);
+
+console.log(pessoa5);*/
