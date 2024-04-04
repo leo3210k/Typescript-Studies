e6930a53a67c302fe2667ff8afc5ef4795e58b3f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-04 16:33:24 -0300

    add math file

---
+++ Modulo 9/introModules.ts/math.ts
@@ -0,0 +1,16 @@
+// ==> Exemplo 01: Modules
+export function somar(x: number, y: number): number {
+  return x + y;
+}
+
+export function subtrair(x: number, y: number): number {
+  return x - y;
+}
+
+export function multiplicar(x: number, y: number): number {
+  return x * y;
+}
+
+export function dividir(x: number, y: number): number {
+  return x / y;
+}
